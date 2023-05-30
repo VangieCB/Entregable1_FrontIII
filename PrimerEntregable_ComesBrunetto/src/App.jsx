0c8f72e41510c14bb166fa03b81d390d703b2ea2
VangieCB <ecomes@hotmail.com>
2023-05-29 23:03:18 -0300

    agregue el form

--- PrimerEntregable_ComesBrunetto/src/App.jsx
+++ PrimerEntregable_ComesBrunetto/src/App.jsx
@@ -3,15 +3,22 @@ import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  /*const [alumno, setAlumno] = useState({
+    nombre: '',
+    nota: 0
+  })*/
 
   return (
     <>
     <div className='App'>
-      
-      <h1>App padre</h1>
 
+      <h1>Carga de Estudiantes</h1>
+
+      <form>  </form>
+      
       <Card/>
         
 
